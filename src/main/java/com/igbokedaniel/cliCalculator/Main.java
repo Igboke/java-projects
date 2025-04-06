6a8ef8dbeb191868ada93aee61ffa41266c3e4cc
Igboke Daniel <danieligboke669@gmail.com>
2025-04-06 18:25:10 +0100

    Modified structure of programme added while loop

--- src/main/java/com/igbokedaniel/cliCalculator/Main.java
+++ src/main/java/com/igbokedaniel/cliCalculator/Main.java
@@ -4,45 +4,65 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1;
-        double num2;
-        char operator;
+        double num1 =0.0;
+        double num2=0.0;
+        char operator=' ';
         double result=0;
+        boolean endProgram = false;
+        boolean validInput;
 
-        
-        try {
-            System.out.print("Enter Number: ");
-            num1 = scanner.nextDouble();
+        while (!endProgram) {
+            validInput = false;
+            while (!validInput) {
+                System.out.print("Enter Number: ");
+                try {
+                    num1 = scanner.nextDouble();
+                    validInput = true;   
+                } catch (java.util.InputMismatchException e) {
+                    System.out.println("Enter a valid Integer");
+                    scanner.next();
+                }
+            }
             
-        } catch (Exception e) {
-            System.out.println("Enter a valid Integer");
-            scanner.close();
-            return;
-        }
-
-        System.out.print("Enter Operator *, +, -, /, ^: ");
-        operator = scanner.next().charAt(0);
-        
-        try {
-            System.out.print("Enter Number: ");
-            num2 = scanner.nextDouble();
+            System.out.print("Enter Operator *, +, -, /, ^, Q to quit: ");
+            operator = scanner.next().charAt(0);
             
-        } catch (Exception e) {
-            System.out.println("Enter a valid Integer");
-            scanner.close();
-            return;
-        }
-        switch (operator) {
-            case '+': result = num1 + num2; break;
-            case '-': result = num1 - num2; break;
-            case '*': result = num1*num2; break;
-            case '/': result = num1/num2; break;
-            case '^': result = Math.pow(num1, num2); break;
-            default:
-                System.out.println("Invalid Operator");
-        }
-        scanner.close();
-        System.out.printf("The result of %.1f %c %.1f is %.1f",num1,operator,num2,result);
+            if (operator == 'q' || operator == 'Q'){
+                endProgram = true;
+                continue;
+            }
+            
+            validInput = false;
+            while (!validInput) {
+                System.out.print("Enter Number: ");
+                try {
+                    num2 = scanner.nextDouble();
+                    validInput = true;
+                } catch (java.util.InputMismatchException e) {
+                    System.out.println("Enter a valid Integer");
+                    scanner.next();
+                }
+            }
+            
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1*num2; break;
+                case '/': {
+                    if(num2 == 0){
+                        System.out.println("Cannot Divide by 0");
+                        continue;
+                    }
+                    result = num1/num2; 
+                    break;}
+                case '^': result = Math.pow(num1, num2); break;
+                default:
+                    System.out.println("Invalid Operator");
+                    continue;
+            }
+            System.out.printf("The result of %.1f %c %.1f is %.1f\n",num1,operator,num2,result);
+    }
+        scanner.close();    
         
     }
 }
